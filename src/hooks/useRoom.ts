85d65b6c493b34857451b1a1ec5a240703b6b659
juliocma <julio@outlook.com.br>
2021-07-15 20:57:14 -0300

    day 4 end

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { database } from '../services/firebase';
 import { useAuth } from './useAuths';
 
@@ -35,6 +36,8 @@ type QuestionType = {
 
 export function useRoom(roomID: string) {
   const { user } = useAuth();
+  const history = useHistory();
+
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -43,7 +46,11 @@ export function useRoom(roomID: string) {
 
     roomRef.on('value', (room) => {
       const databaseRoom = room.val();
-
+      if (databaseRoom.closedAt) {
+        alert('Room already closed');
+        history.push('/');
+        return;
+      }
       const firebaseQuestions: FirebaseQuestions =
         databaseRoom?.questions ?? {};
       const parsedQuestions = Object.entries(firebaseQuestions).map(
@@ -61,6 +68,7 @@ export function useRoom(roomID: string) {
           };
         }
       );
+
       setTitle(databaseRoom?.title);
       setQuestions(parsedQuestions);
     });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,65 +1,69 @@
-import { FormEvent, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { database } from "../services/firebase";
+import { FormEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { database } from '../services/firebase';
 
-import { useAuth } from "../hooks/useAuths";
+import { useAuth } from '../hooks/useAuths';
 
-import illustrationImg from "../assets/images/illustration.svg";
-import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
 
-import "../styles/auth.scss";
-import { Button } from "../components/Button";
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
 
 export function Home() {
   const history = useHistory();
   const { user, signWithGoogle } = useAuth();
-  const [roomCode, setRoomCode] = useState("");
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
       await signWithGoogle();
     }
 
-    history.push("/rooms/new");
+    history.push('/rooms/new');
   }
   async function handleJoinRoom(event: FormEvent) {
     event.preventDefault();
-    if (roomCode.trim() === "") {
+    if (roomCode.trim() === '') {
       return;
     }
 
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
     if (!roomRef.exists()) {
-      alert("Room does not exists");
+      alert('Room does not exists');
+      return;
+    }
+    if (roomRef.val().closedAt) {
+      alert('Room already closed');
       return;
     }
     history.push(`/rooms/${roomCode}`);
   }
 
   return (
-    <div id="page-auth">
+    <div id='page-auth'>
       <aside>
-        <img src={illustrationImg} alt="Ilustração" />
+        <img src={illustrationImg} alt='Ilustração' />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Logo" />
-          <Button onClick={handleCreateRoom} color="secondary">
-            <img src={googleIconImg} alt="Google" />
+        <div className='main-content'>
+          <img src={logoImg} alt='Logo' />
+          <Button onClick={handleCreateRoom} color='secondary'>
+            <img src={googleIconImg} alt='Google' />
             Crie sua sala com o Google
           </Button>
-          <div className="separator">ou entre em uma sala</div>
+          <div className='separator'>ou entre em uma sala</div>
           <form onSubmit={handleJoinRoom}>
             <input
-              type="text"
-              placeholder="Digite o código da sala"
+              type='text'
+              placeholder='Digite o código da sala'
               value={roomCode}
               onChange={(event) => setRoomCode(event.target.value)}
             />
-            <Button type="submit">Entrar na sala</Button>
+            <Button type='submit'>Entrar na sala</Button>
           </form>
         </div>
       </main>
